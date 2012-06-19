cc2bd7034565c381b8cab7ba34790aca4a16d6d4
anpefi <anpefi@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-19 18:21:59 +0000

    A lot of changes and fixes. Added extra arguments to msap(). Added nj tree. R CMD checks OK.

--- pkg/msap/R/pcoa.R
+++ pkg/msap/R/pcoa.R
@@ -16,15 +16,15 @@ pcoa <- function(DM, groups,name,surname){
 	maxY <- max(pcol$li$A2)
 	minY <- min(pcol$li$A2)
 	fullname <- paste(name,surname, sep='-')
-	png(filename=paste(fullname,"png",sep='.'), width=680, height=680)
+	png(filename=paste(fullname,"png",sep='.'))
 	par(bty = 'n')
 	plot(0,0, main=paste(name,surname, sep=": "), type = "n",xlab=paste("C1 (",var1,"%)"),ylab=paste("C2 (",var2,"%)"), xlim=c(minX,maxX+0.1), ylim=c(minY,maxY+0.1), frame=TRUE, cex=1.5)
-	bgcolors<-c("black", "green", "yellow", "red", "blue", "orange", "pink")
-	symbs <- c(21,22,23,24,25,26,27) #What if we have > 7 groups???
+	bgcolors<-rainbow(ntt+1)[-2] #No yellow?
+	symbs <- c(21,22,23,24,25) #What if we have > 7 groups???
 	for(i in 1:ntt){
-		points(spcoo[[i]], pch=symbs[i], col="black", bg=bgcolors[i])
+		points(spcoo[[i]], pch=21, col="black", bg=bgcolors[i])
 	}	
-	s.class(pcol$li, groups, cpoint=0, add.plot=TRUE)
+	s.class(pcol$li, groups, cpoint=0, col=bgcolors, add.plot=TRUE)
 	dev.off()
 
 }
