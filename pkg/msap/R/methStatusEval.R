9d3157995e2406dab766e1295a3e8120d003a6b1
anpefi <andres.perez.figueroa@gmail.com>
2017-02-03 12:19:22 +0100

    Fix MSL classification when there are NAs in data
    When there was NA in the data file, these were taken as NA in the dataMIX. After, when assessing
    fragment classification into MSL or NML, these NA where not taken into account. In this scenario,
    if a locus for 10 samples, under the uninformative option, had 9 0/0 and 1 NAs then it was
    missclassified as MSL beacuse it was calculating frequency of methylation states as 1-9/10=0.1
    (>0.05 threshold, MSL), when it should be 1-9/9=0 (<0.05, NML).
    
    This missclassifation was also causing that were, when transformed, MSL with all NAs, causing
    that polymorphic() yielded NULL values, crashing the run (as reported first by Rik Lievers).
    
    Changing the denominator in the calculation of methylation states frequency in methStatusEval.R
    solved the problem.
    
    Fix #10

--- pkg/msap/R/methStatusEval.R
+++ pkg/msap/R/methStatusEval.R
@@ -4,8 +4,8 @@ methStatusEval <- function(x, error=0.05, uninformative=TRUE){
 	threshold= ((error + error) - (2*error*error))
 	#A locus is classified as "methylation-susceptible" if the frecuency of methylation evidence (10, 1 or 0) is above threshold
 	#If 00 pattern assumes uninformative state. 
-	if(uninformative) fMet <- 1-(length(x[which(x==11|x==0)])/length(x)) 
+	if(uninformative) fMet <- 1-(length(x[which(x==11|x==0)])/sum(!is.na(x)) ) 
 	# If 00 patter assumes hypermethylation 
-	else fMet <- 1-(length(x[which(x==11)])/length(x))
+	else fMet <- 1-(length(x[which(x==11)])/sum(!is.na(x)) )
 	return(fMet>threshold)
 }
