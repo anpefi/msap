0a1cd404d545059cd06033764523c3fb09295efc
anpefi <andres.perez.figueroa@gmail.com>
2018-10-29 15:17:04 +0100

    Allow user-defined threshold to define loci as polymorphic
    With this update there are two new arguments, threshold_poly_MSL and threshold_poly_NML to define the minimum number of ocurrences of each state (methylated/unmethylated for MSL, band/no band for NML) needed to define a locus polymorphic. There were an unconsistency as the manual said that value used to be 2, but that was changed in very early versions of msap to 1.
    Closes #16

--- NEWS.md
+++ NEWS.md
@@ -1,6 +1,12 @@
-# msap 1.2.0.9000
+# msap 1.2.0.9001
 
-* Added a `NEWS.md` file to track changes to the package.
+## Major Changes
+* Definition of polymorphic sites now allows for user-defined thresholds with arguments for both MSL and NML threshold_poly_MSL and threshold_poly_NML
 
+## Minor Changes
+* Add this `NEWS.md` file to track changes to the package.
 
+## Bug fixes
+* Fix bug causing Mantel test failed when not doing cluster before
+* Fix MSL classification when there are NAs in data (#10)
 
--- R/polymorphic.R
+++ R/polymorphic.R
@@ -1,15 +1,16 @@
 #polymorphic.R
 # Andrés Pérez-Figueroa
 
-# From a binary matrix returns a list with indices of those polymorphic columns (i.e. at least two ocurrences for every state)
-polymorphic <- function(col){
+# From a binary matrix returns a list with indices of those polymorphic columns.
+# Polymorphic threshold is now user defined (By default at least two ocurrences for every state)
+polymorphic <- function(col, threshold=1){
 	tcol <- table(col)
 	if(length(tcol)>2){
 		cat("ERROR! data matrix in 'polymorphic' is not binary\n")
 	} 
 	if(length(tcol)==1) return(FALSE)
 	if(length(tcol)==2){
-		if(min(tcol)>0) return (TRUE)
+		if(min(tcol)>threshold-1) return (TRUE)
 		else return (FALSE)
 	}
 }
