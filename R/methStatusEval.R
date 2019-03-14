35d0555d6394131242e5f234ab5aab15fb81acec
anpefi <andres.perez.figueroa@gmail.com>
2019-03-14 12:33:37 +0100

    Change methStatusEval function to deal with pattern argument
    As a new global pattern argument would be used to define the methylation patterns (see #17), methStatusEval should be changed to deal with it and report the methylation state of a locus dependending on the specified pattern

--- R/methStatusEval.R
+++ R/methStatusEval.R
@@ -1,11 +1,12 @@
-methStatusEval <- function(x, error=0.05, uninformative=TRUE){
+methStatusEval <- function(x, error=0.05, pattern=c(1,2,2,NA)){
 	#Evaluate for classification as 'methylation-susceptible locus' or 'non methylated"
 	#Uses mixed data: 11, 10, 1, 0 are the four possible states for both enzymes
 	threshold= ((error + error) - (2*error*error))
-	#A locus is classified as "methylation-susceptible" if the frecuency of methylation evidence (10, 1 or 0) is above threshold
-	#If 00 pattern assumes uninformative state. 
-	if(uninformative) fMet <- 1-(length(x[which(x==11|x==0)])/sum(!is.na(x)) ) 
-	# If 00 patter assumes hypermethylation 
-	else fMet <- 1-(length(x[which(x==11)])/sum(!is.na(x)) )
-	return(fMet>threshold)
+	#A locus is classified as "methylation-susceptible" if the frecuency of methylation evidence (2 in pattern) is above threshold
+	
+	#define the patterns methylated (2)
+	counts <- as.numeric(table(factor(x,levels = c(11,10,1,0)) ) )
+	fmet <- sum(counts[pattern==2],na.rm = T) / sum(counts[!is.na(pattern)],na.rm = T)
+	
+	return(fmet > threshold)
 }
